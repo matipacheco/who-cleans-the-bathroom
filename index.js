acb535c40e1f57325d6337f1e7cdd94707d398ec
Mati <matipacheco@gmail.com>
2019-11-12 20:56:52 -0300

    node version

---
+++ index.js
@@ -0,0 +1,24 @@
+const accountSid    = process.env['TWILIO_ACCOUNT_SID'];TWILIO_PHONE_NUMBER
+const authToken     = process.env['TWILIO_AUTH_TOKEN'];
+const twilioClient  = require('twilio')(accountSid, authToken);
+
+const _MS_PER_DAY = 1000 * 60 * 60 * 24;
+
+function getReceiver(today, startDate) {
+  return (Math.floor((today - startDate) / _MS_PER_DAY) % 2 === 0) ? 'EVEN' : 'ODD'
+}
+
+exports.handler = async function(event) {
+  let today     = new Date();
+  let startDate = new Date(process.env['START_DATE']);
+
+  twilioClient.messages
+    .create({
+      from: 'whatsapp:' + process.env['TWILIO_PHONE_NUMBER'],
+      to: 'whatsapp:' + process.env[getReceiver(today, startDate) + '_PHONE_NUMBER'],
+      body: 'Your ' + '🚽' + ' code is ' + '💩'
+    })
+    .then(message => {
+      console.log(message.sid)
+    });
+};
