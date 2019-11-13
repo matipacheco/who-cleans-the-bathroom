7d2bca5b6320ac8a74e77f778a6b1e60a8ed4afc
Mati <matipacheco@gmail.com>
2019-11-12 21:03:37 -0300

    unknown variable

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const accountSid    = process.env['TWILIO_ACCOUNT_SID'];TWILIO_PHONE_NUMBER
+const accountSid    = process.env['TWILIO_ACCOUNT_SID'];
 const authToken     = process.env['TWILIO_AUTH_TOKEN'];
 const twilioClient  = require('twilio')(accountSid, authToken);
 
